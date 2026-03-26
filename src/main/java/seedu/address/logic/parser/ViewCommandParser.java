c67fdfb5fb48951fbaa8c7164c56d70198258920
Ryan Goh <ryangoh206@gmail.com>
2026-03-26 12:17:11 +0800

    Add ViewCommand tests and fix broken tests

--- src/main/java/seedu/address/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -10,14 +12,19 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class ViewCommandParser implements Parser<ViewCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the ViewCommand
-     * and returns a ViewCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the ViewCommand and returns a
+     * ViewCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
     @Override
     public ViewCommand parse(String args) throws ParseException {
-        Index index = ParserUtil.parseIndex(args);
-        return new ViewCommand(index);
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new ViewCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE), pe);
+        }
     }
 }
